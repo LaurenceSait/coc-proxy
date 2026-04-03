766c32b15f56d3c31d447aff75e9ec8b3fe687c1
LaurenceSait <laurence.sait23@gmail.com>
2026-04-03 12:18:07 +0100

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -31,3 +31,5 @@ export default async function handler(req, res) {
     return res.status(response.status).json(data);
   } catch (err) {
     return res.status(500).json({ error: 'Proxy error', detail: err.message });
+  }
+}
